7833f1b025402c5abeee4808e4838ec19fa852e6
dgoceva <dgoceva@abv.bg>
2021-10-21 09:15:28 +0300

    function with param

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
-function getTitle() {
-  return 'React';
+function getTitle(title) {
+  return title;
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello {getTitle()}</h1>
+      <h1>Hello {getTitle('React')}</h1>
 
       <label htmlFor="search">Search:</label>
       <input type="text" id="search" placeholder="Search" />
