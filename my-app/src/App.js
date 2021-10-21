c88d5b78840c57326df565cba39d5db3714a8d31
dgoceva <dgoceva@abv.bg>
2021-10-21 09:05:26 +0300

    search added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello {title}!</h1>
+
+      <label htmlFor="search">Search:</label>
+      <input type="text" id="search" placeholder="Search" />
     </div>
   );
 }
