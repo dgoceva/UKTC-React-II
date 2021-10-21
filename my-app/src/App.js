8ef4c21855538e83c50fb10391fd5acf88f2c5cf
Daniela Gotseva <dgoceva@tu-sofia.bg>
2021-10-21 12:31:57 +0300

    new components added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,60 @@
-import './App.css';
+import "./App.css";
 
-const list=[
+const list = [
   {
-    title: 'React',
-    url: 'http://reactjs.org',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "http://reactjs.org",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'http://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "http://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
-]
+];
 
 function App() {
   return (
     <div>
       <h1>My Stories</h1>
-
-      <label htmlFor="search">Search:</label>
-      <input type="text" id="search" placeholder="Search" />
-      <hr/>
-      <ul>
-        {list.map(function(item){
-          return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-              {" "}
-            </span>
-            <span>{item.author}&nbsp;</span>
-            <span>{item.num_comments+" "}</span>
-            <span>{item.points}</span>
-          </li>
-        )})}
-      </ul>
+      <Search />
+      <hr />
+      <List />
     </div>
   );
 }
 
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input type="text" id="search" placeholder="Search" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>{" "}
+            </span>
+            <span>{item.author}&nbsp;</span>
+            <span>{item.num_comments + " "}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 export default App;
