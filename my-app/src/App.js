9647d5d289a2835165ca05be6c70ffd628684f46
dgoceva <dgoceva@abv.bg>
2021-10-21 09:21:14 +0300

    list added - 07-10-21

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 
-function getTitle(title) {
-  return title;
-}
+const list=[
+  {
+    title: 'React',
+    url: 'http://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Stories</h1>
 
       <label htmlFor="search">Search:</label>
       <input type="text" id="search" placeholder="Search" />
