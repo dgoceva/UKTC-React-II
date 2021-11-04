519630afc603a0d7f29c3cf42410324c6235b3eb
dgoceva <dgoceva@abv.bg>
2021-11-04 12:57:36 +0200

    callback handler added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,21 +21,26 @@ const App = () => {
     },
   ];
 
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Stories</h1>
-      <Search />
+      <Search onSearcher={handleSearch} />
       <hr />
       <List list={stories} />
     </div>
   );
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState("");
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+    props.onSearcher(event);
   };
 
   return (
